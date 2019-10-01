d9b131ab0e199d6e0c649eb6a8b073e5e3eb7c4c
Zigmas Bitinas <zigmbit@gmail.com>
2019-10-01 11:46:11 +0300

    Add documentation

--- GameState.cs
+++ GameState.cs
@@ -25,27 +25,38 @@ namespace Game1000
             camera = new Camera();
         }
 
-        public void Update(GameTime gameTime)
+         public void Update(GameTime gameTime)
         {
+            // Elapsed time since the last update
             float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Update the player positions if they are colliding
+            // (Current implementation may cause problems for >2 players)
+            // TODO: test this out
             for (int i = 0; i < players.Count; i++)
                 for (int j = 0; j < i; j++)
                     Player.Collide(players[i], players[j]);
 
+            // Check if any player has been shot
             foreach (Player player in players)
                 foreach (Bullet bullet in bullets)
                     Bullet.Collide(player, bullet);
 
+            // Check if any 2 bullets have collided
             for (int i = 0; i < bullets.Count; i++)
                 for (int j = 0; j < i; j++)
                     Bullet.Collide(bullets[i], bullets[j]);
 
+            // Update the position of each player based on the time that has elapsed
+            // TODO: pass the controls of a player as a default  
             foreach (Player player in players)
                 player.Update(elapsed, arena.radius, bullets);
 
+            // Update position of bullets
             foreach (Bullet bullet in bullets)
                 bullet.Update(elapsed, arena.radius);
 
+            // Remove dead players
             for (int i = 0; i < players.Count; i++)
                 if (!players[i].isAlive)
                 {
@@ -53,6 +64,7 @@ namespace Game1000
                     i--;
                 }
 
+            // Remove bullets that have exploded
             for (int i = 0; i < bullets.Count; i++)
                 if (!bullets[i].isAlive)
                 {
@@ -60,6 +72,8 @@ namespace Game1000
                     i--;
                 }
 
+            // Update the arena
+            // Currently this means shrinking the size
             arena.Update(elapsed);
         }
 
