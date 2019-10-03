ff3d8d3e10989a0711f45692c34d772cdf8d56ec
Zigmas Bitinas <zigmbit@gmail.com>
2019-10-04 01:27:13 +0300

    Created skeleton of client connecting to server

--- Game1.cs
+++ Game1.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Lidgren.Network;
 
 namespace Game1000
 {
@@ -11,6 +13,11 @@ namespace Game1000
         SpriteBatch spriteBatch;
 
         GameState game;
+
+        NetClient client;
+
+        Dictionary<long, Controls> controls;
+        Dictionary<long, Player> players;
 
         public Game1()
         {
@@ -25,6 +32,14 @@ namespace Game1000
         protected override void Initialize()
         {
             base.Initialize();
+
+            // Initialize and start client
+            NetPeerConfiguration config = new NetPeerConfiguration("sick_game");
+            client = new NetClient(config);
+            client.Start();
+
+            // Connect to server
+            client.Connect("localhost", 14242);
         }
 
         protected override void LoadContent()
@@ -32,6 +47,8 @@ namespace Game1000
             spriteBatch = new SpriteBatch(GraphicsDevice);
             C.Content = Content;
             game = new GameState();
+            controls = new Dictionary<long, Controls>();
+            players = new Dictionary<long, Player>();
         }
 
         protected override void UnloadContent()
@@ -43,6 +60,33 @@ namespace Game1000
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            // TODO: check for changes to controls here
+            
+            NetIncomingMessage msg;
+            while ((msg = client.ReadMessage()) != null)
+            {
+                switch (msg.MessageType)
+                {
+                    case NetIncomingMessageType.VerboseDebugMessage:
+                    case NetIncomingMessageType.DebugMessage:
+                    case NetIncomingMessageType.WarningMessage:
+                    case NetIncomingMessageType.ErrorMessage:
+                        Console.WriteLine(msg.ReadString());
+                        break;
+                    case NetIncomingMessageType.StatusChanged:
+                        
+
+                        break;
+                    case NetIncomingMessageType.Data:
+                        break;
+                    default:
+                        Console.WriteLine("Unhandled type: " + msg.MessageType);
+                        break;
+                }
+                client.Recycle(msg);
+            }
+
+            // Update game after receiving input changes
             game.Update(gameTime);
 
             base.Update(gameTime);
