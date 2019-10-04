f23b585cf84ee9dd0738a46a859203b8282477d2
Zigmas Bitinas <zigmbit@gmail.com>
2019-10-04 15:47:58 +0300

    Fix Controls decoding issue

--- NetworkingUtilities.cs
+++ NetworkingUtilities.cs
@@ -1,6 +1,7 @@
 using Lidgren.Network;
 using Game1000;
 using Microsoft.Xna.Framework;
+using System;
 
 namespace Networking{
     class Utilities{
@@ -28,19 +29,19 @@ namespace Networking{
             b ^= (byte)((c.mouseRight ? 1 : 0) << 5);
             msg.Write(b);
             if(c.mouseLeft || c.mouseRight){
-                // Send mouse coordinates as well
+                // Get mouse coordinates as well
                 msg.Write(c.mousePos.X);
                 msg.Write(c.mousePos.Y);
             }
         }
         public static void DecodeControls(NetIncomingMessage msg, Controls c){
             byte b = msg.ReadByte();
-            c.up = (b & 1<<0) == 1 ? true : false;
-            c.left = (b & 1<<1) == 1 ? true : false;
-            c.down = (b & 1<<2) == 1 ? true : false;
-            c.right = (b & 1<<3) == 1 ? true : false;
-            c.mouseLeft = (b & 1<<4) == 1 ? true : false;
-            c.mouseRight = (b & 1<<5) == 1 ? true : false;
+            c.up = (b & 1<<0) > 0;
+            c.left = (b & 1<<1) > 0;
+            c.down = (b & 1<<2) > 0;
+            c.right = (b & 1<<3) > 0;
+            c.mouseLeft = (b & 1<<4) > 0;
+            c.mouseRight = (b & 1<<5) > 0;
             if(c.mouseLeft || c.mouseRight){
                 // Send mouse coordinates as well
                 int x = msg.ReadInt32();
