09344683a65f29339f08611abb7c938a14e91cb2
Zigmas Bitinas <zigmbit@gmail.com>
2019-10-04 03:42:43 +0300

    Add default values to Controls auto-properties

--- controls/Controls.cs
+++ controls/Controls.cs
@@ -6,12 +6,12 @@ namespace Game1000
     // By default the Mouse and Keyboard state can be changed manually to allow remote execution of
     public class Controls
     {
-        public virtual bool up {get; set;}
-        public virtual bool down {get; set;}
-        public virtual bool right {get; set;}
-        public virtual bool left {get; set;}
-        public virtual Vector2 mousePos {get; set;}
-        public virtual bool mouseRight {get; set;}
-        public virtual bool mouseLeft {get; set;}
+        public virtual bool up {get; set;} = false;
+        public virtual bool down {get; set;} = false;
+        public virtual bool right {get; set;} = false;
+        public virtual bool left {get; set;} = false;
+        public virtual Vector2 mousePos {get; set;} = Vector2.Zero;
+        public virtual bool mouseRight {get; set;} = false;
+        public virtual bool mouseLeft {get; set;} = false;
     }
 }
